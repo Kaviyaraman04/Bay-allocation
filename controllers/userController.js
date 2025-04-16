ab6b2e37d2da0734f288d794ab965521583f0b43
kaviya <kaviyaramank@gmail.com>
2025-04-16 18:28:26 +0530

    updated messgage added

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,18 +37,22 @@ exports.getUserById = async (req, res) => {
 
 // Update user (SHOW password)
 exports.updateUser = async (req, res) => {
-  const { user_id, ...updateData } = req.body;
-  if (!user_id) return res.status(400).json({ message: 'User ID is required to update.' });
-
-  try {
-    const updated = await User.findByIdAndUpdate(user_id, updateData, { new: true }); // no .select()
-    if (!updated) return res.status(404).json({ message: 'User not found' });
-    res.status(200).json(updated);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
+    const { user_id, ...updateData } = req.body;
+    if (!user_id) return res.status(400).json({ message: 'User ID is required to update.' });
+  
+    try {
+      const updated = await User.findByIdAndUpdate(user_id, updateData, { new: true });
+      if (!updated) return res.status(404).json({ message: 'User not found' });
+  
+      res.status(200).json({
+        message: 'User updated successfully',
+        updatedUser: updated
+      });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
 // Delete user
 exports.deleteUser = async (req, res) => {
   const { userid } = req.query;
